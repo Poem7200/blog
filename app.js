11d7866996345460415fd237b8b95693781ccd4c
shige <shige@aecg.com.cn>
2020-07-20 05:59:24 +0800

    postData处理

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ const querystring = require('querystring');
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
 
+// 处理post data
+const getPostData = req => {
+  const promise = new Promise((resolve, reject) => {
+    if (req.method !== 'POST') {
+      // 如果非post方法，不需要报错，返回一个空的对象即可
+      resolve({});
+      return;
+    }
+    // 请求头不是json格式，返回空的对象
+    if (req.headers['content-type'] !== 'application/json') {
+      resolve({});
+      return;
+    }
+    let postData = '';
+    req.on('data', chunk => {
+      postData += chunk.toString();
+    });
+    req.on('end', () => {
+      if (!postData) {
+        resolve({});
+        return;
+      }
+      resolve(JSON.parse(postData));
+    })
+  });
+  return promise;
+}
+
 const serverHandle = (req, res) => {
   // 设置返回格式为JSON
   res.setHeader('Content-type', 'application/json');
@@ -14,24 +42,30 @@ const serverHandle = (req, res) => {
   // 解析query
   req.query = querystring.parse(url.split('?')[0]);
 
-  // 处理blog路由
-  const blogData = handleBlogRouter(req, res);
-  if (blogData) {
-    res.end(JSON.stringify(blogData));
-    return;
-  }
+  // 处理post data
+  getPostData(req).then(postData => {
+    // 获取postData后，把值放到请求体
+    req.body = postData;
 
-  // 处理user路由
-  const userData = handleUserRouter(req, res);
-  if (userData) {
-    res.end(JSON.stringify(userData));
-    return;
-  }
+    // 处理blog路由
+    const blogData = handleBlogRouter(req, res);
+    if (blogData) {
+      res.end(JSON.stringify(blogData));
+      return;
+    }
 
-  // 未命中路由，返回404
-  res.writeHead(404, {'Content-type': 'text/plain'});
-  res.write('404 Not Found\n');
-  res.end();
+    // 处理user路由
+    const userData = handleUserRouter(req, res);
+    if (userData) {
+      res.end(JSON.stringify(userData));
+      return;
+    }
+
+    // 未命中路由，返回404
+    res.writeHead(404, { 'Content-type': 'text/plain' });
+    res.write('404 Not Found\n');
+    res.end();
+  });
 }
 
 module.exports = serverHandle;
